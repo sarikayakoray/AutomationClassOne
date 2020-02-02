3c086ab74fd5ef7ae6bf4bccca8158e9e7e8e9e0
sarik <sarik@192.168.86.196>
2020-02-02 16:46:31 -0500

    First_push_Feb_2nd

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,36 @@
+package com.DataTypePackage;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//	integer datatype
+	int x = 1; 
+	int y = 3;
+	int j = 3; 
+	x = 5;
+	
+	//double datatype
+	double a = 1.5;
+	double d = 2.5;
+	
+	//character datatype
+	char z = 'b';
+	
+	//String a non primitive data type. String is a class.
+	String name = "Shomir";
+	
+	//boolean datatype
+	
+	boolean b = true; 
+	boolean c = false;
+	
+	
+	
+	System.out.println("My name is " + name);
+	}  
+	
+	
+
+}
